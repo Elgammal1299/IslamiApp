0599d444277ab5cbd122b9258c83a91ac173161a
Ahmed Elgammal <elgammal2024@gmail.com>
2025-07-10 20:04:23 +0300

    feat: Update RadioPage and RadioPlayerPage UI for improved theming and consistency

--- lib/feature/home/ui/view/radio_page.dart
+++ lib/feature/home/ui/view/radio_page.dart
@@ -16,125 +16,105 @@ class _RadioPageState extends State<RadioPage> {
     super.initState();
     context.read<RadioCubit>().loadRadioStations();
   }
- 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'إذاعة القرآن',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-        ),
-        centerTitle: true,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.teal.shade50, Colors.white],
-          ),
-        ),
-        child: BlocBuilder<RadioCubit, RadioState>(
+      appBar: AppBar(title: const Text('إذاعة القرآن'), centerTitle: true),
+      body: BlocBuilder<RadioCubit, RadioState>(
         //  buildWhen: (previous, current) {
         //       // إعادة البناء فقط عند تغيير حالة التشغيل
         //       return current is RadioPlaying ||
         //           (previous is RadioPlaying && current is! RadioPlaying);
         //     },
-          builder: (context, state) {
-            if (state is RadioLoading) {
-              return const Center(child: CircularProgressIndicator());
-            }
+        builder: (context, state) {
+          if (state is RadioLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-            if (state is RadioError) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(state.message),
-                    const SizedBox(height: 16),
-                    ElevatedButton(
-                      onPressed: () {
-                        context.read<RadioCubit>().loadRadioStations();
-                      },
-                      child: const Text('إعادة المحاولة'),
-                    ),
-                  ],
-                ),
-              );
-            }
+          if (state is RadioError) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(state.message),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.read<RadioCubit>().loadRadioStations();
+                    },
+                    child: const Text('إعادة المحاولة'),
+                  ),
+                ],
+              ),
+            );
+          }
 
-            if (state is RadioLoaded) {
-              return ListView.builder(
-                padding: const EdgeInsets.all(16),
-                itemCount: state.stations.length,
-                itemBuilder: (context, index) {
-                  final station = state.stations[index];
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 12),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          AppRoutes.radioPlayerRouter,
-                          arguments: {
-                            "station": station,
-                       'radioCubit': context.read<RadioCubit>(),
-                          },
-                        );
-                    
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 2,
-                              blurRadius: 8,
-                              offset: const Offset(0, 3),
-                            ),
-                          ],
+          if (state is RadioLoaded) {
+            return ListView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: state.stations.length,
+              itemBuilder: (context, index) {
+                final station = state.stations[index];
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        AppRoutes.radioPlayerRouter,
+                        arguments: {
+                          "station": station,
+                          'radioCubit': context.read<RadioCubit>(),
+                        },
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Theme.of(context).secondaryHeaderColor,
+                            spreadRadius: 1,
+                            blurRadius: 2,
+                            offset: const Offset(0, 1),
+                          ),
+                        ],
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(16),
+                        leading: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.person,
+                            color: Theme.of(context).primaryColorDark,
+                          ),
                         ),
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.all(16),
-                          leading: Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.teal.shade100,
-                              shape: BoxShape.circle,
-                            ),
-                            child: Icon(
-                              Icons.person,
-                              color: Colors.teal.shade700,
-                            ),
-                          ),
-                          title: Text(
-                            station.name,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          trailing: Icon(
-                            Icons.play_circle_filled,
-                            color: Colors.teal.shade300,
-                            size: 40,
-                          ),
+                        title: Text(
+                          station.name,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        trailing: Icon(
+                          Icons.play_circle_filled,
+                          color: Theme.of(context).primaryColor,
+                          size: 40,
                         ),
                       ),
                     ),
-                  );
-                },
-              );
-            }
+                  ),
+                );
+              },
+            );
+          }
 
-            return const SizedBox();
-          },
-        ),
+          return const SizedBox();
+        },
       ),
     );
   }
