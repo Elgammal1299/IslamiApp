929d8c18914e861e5cccf9a33f443292b32091e5
Ahmed Elgammal <elgammal2024@gmail.com>
2025-07-18 15:38:59 +0300

    feat: Replace PNG images with WEBP format for improved performance and reduced file size

--- lib/core/constant/app_image.dart
+++ lib/core/constant/app_image.dart
@@ -1,22 +1,22 @@
 class AppImage {
-  static final basmalaImage = "assets/images/Basmala.png";
-  static final surahFramImage = "assets/images/surah_fram.png";
-  static final quranImage = "assets/images/quran.png";
-  static final hadithImage = "assets/images/hadith_image.png";
-  static final beadsImage = "assets/images/beads.png";
-  static final kaabaImage = "assets/images/kaaba.png";
-  static final azkarImage = "assets/images/azkar.png";
-  static final prayingImage = "assets/images/praying.png";
-  static final micImage = "assets/images/mic.png";
-  static final audioImage = "assets/images/audio.png";
-  static final radioImage = "assets/images/radio.png";
-  static final splashImageLight = 'assets/images/splash_image_light.png';
-  static final splashImageDark = 'assets/images/splash_image_dark.png';
+  static final basmalaImage = "assets/images/Basmala.webp";
+  static final surahFramImage = "assets/images/surah_fram.webp";
+  static final quranImage = "assets/images/quran.webp";
+  static final hadithImage = "assets/images/hadith_image.webp";
+  static final beadsImage = "assets/images/beads.webp";
+  static final kaabaImage = "assets/images/kaaba.webp";
+  static final azkarImage = "assets/images/azkar.webp";
+  static final prayingImage = "assets/images/praying.webp";
+  static final micImage = "assets/images/mic.webp";
+  static final audioImage = "assets/images/audio.webp";
+  static final radioImage = "assets/images/radio.webp";
+  static final splashImageLight = 'assets/images/splash_image_light.webp';
+  static final splashImageDark = 'assets/images/splash_image_dark.webp';
   static final congratulationsImage =
-      'assets/images/congratulations_images.png';
-  static final gpsImage = 'assets/images/gps.png';
+      'assets/images/congratulations_images.webp';
+  static final gpsImage = 'assets/images/gps.webp';
   static final compassDarkImage = 'assets/images/CompassDark.svg';
   static final compassImage = 'assets/images/Compass.svg';
   static final needleImage = 'assets/images/needle.svg';
-  static final quranCoverImage = 'assets/images/quran_cover.jpg';
+  static final quranCoverImage = 'assets/images/quran_cover.webp';
 }
