43942c310c7900e48290b5139f3228d883ad24b9
ahmed <aabdelsalam854@gmail.com>
2025-07-27 00:15:39 +0300

    refactor: Simplify bookmark management by using server locator for SharedPreferences access

--- lib/core/services/bookmark_manager.dart
+++ lib/core/services/bookmark_manager.dart
@@ -1,3 +1,4 @@
+import 'package:islami_app/core/services/server_locator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class BookmarkManager {
@@ -5,33 +6,32 @@ class BookmarkManager {
 
   // إضافة آية للمفضلة
   static Future<void> addBookmark(int surah, int ayah) async {
-    final prefs = await SharedPreferences.getInstance();
-    List<String> bookmarks = prefs.getStringList(_bookmarksKey) ?? [];
+    List<String> bookmarks =
+        sl<SharedPreferences>().getStringList(_bookmarksKey) ?? [];
     String bookmark = '$surah:$ayah';
     if (!bookmarks.contains(bookmark)) {
       bookmarks.add(bookmark);
-      await prefs.setStringList(_bookmarksKey, bookmarks);
+      await sl<SharedPreferences>().setStringList(_bookmarksKey, bookmarks);
     }
   }
 
   // إزالة آية من المفضلة
   static Future<void> removeBookmark(int surah, int ayah) async {
-    final prefs = await SharedPreferences.getInstance();
-    List<String> bookmarks = prefs.getStringList(_bookmarksKey) ?? [];
+    List<String> bookmarks =
+        sl<SharedPreferences>().getStringList(_bookmarksKey) ?? [];
     bookmarks.remove('$surah:$ayah');
-    await prefs.setStringList(_bookmarksKey, bookmarks);
+    await sl<SharedPreferences>().setStringList(_bookmarksKey, bookmarks);
   }
 
   // التحقق ما إذا كانت الآية في المفضلة
   static Future<bool> isBookmarked(int surah, int ayah) async {
-    final prefs = await SharedPreferences.getInstance();
-    List<String> bookmarks = prefs.getStringList(_bookmarksKey) ?? [];
+    List<String> bookmarks =
+        sl<SharedPreferences>().getStringList(_bookmarksKey) ?? [];
     return bookmarks.contains('$surah:$ayah');
   }
 
   // الحصول على جميع الآيات المفضلة
   static Future<List<String>> getAllBookmarks() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getStringList(_bookmarksKey) ?? [];
+    return sl<SharedPreferences>().getStringList(_bookmarksKey) ?? [];
   }
 }
