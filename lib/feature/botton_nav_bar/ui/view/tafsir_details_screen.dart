90735ef569c2223539adee149bb1343c94df30a1
Ahmed Elgammal <elgammal2024@gmail.com>
2025-09-20 13:52:31 +0300

    fix: Update version number to 1.0.1+3 in pubspec.yaml

--- lib/feature/botton_nav_bar/ui/view/tafsir_details_screen.dart
+++ lib/feature/botton_nav_bar/ui/view/tafsir_details_screen.dart
@@ -33,64 +33,66 @@ class _TafsirDetailsScreenState extends State<TafsirDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("تفسير الآية")),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: Card(
-                color: Theme.of(context).cardColor,
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                shadowColor: AppColors.white,
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    textDirection: TextDirection.rtl,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text("تفسير الآية")),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Card(
+                  color: Theme.of(context).cardColor,
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  shadowColor: AppColors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Text(
+                      textDirection: TextDirection.rtl,
 
-                    widget.text,
-                    style: context.textTheme.titleLarge,
-                    textAlign: TextAlign.justify,
+                      widget.text,
+                      style: context.textTheme.titleLarge,
+                      textAlign: TextAlign.justify,
+                    ),
                   ),
                 ),
-              ),
 
-              // 🔍 عرض التفسير
-            ),
-            const SliverToBoxAdapter(child: SizedBox(height: 16)),
-            SliverToBoxAdapter(
-              child: BlocBuilder<TafsirCubit, TafsirByAyahState>(
-                builder: (context, state) {
-                  if (state is TafsirByAyahError) {
-                    return customErrorWidget(
-                      onPressed: () {
-                        context.read<TafsirCubit>().fetchAyahTafsir(
-                          widget.verse.toString(),
-                          widget.tafsirIdentifier,
-                        );
-                      },
-                    );
-                  } else if (state is TafsirByAyahLoaded) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        textDirection: TextDirection.rtl,
-                        textAlign: TextAlign.justify,
-                        state.tafsirByAyah.data!.text.toString(),
-                        style: context.textTheme.titleLarge,
-                        // textAlign: TextAlign.justify,
-                      ),
-                    );
-                  }
-                  return const Center(child: CircularProgressIndicator());
-                },
+                // 🔍 عرض التفسير
               ),
-            ),
-          ],
+              const SliverToBoxAdapter(child: SizedBox(height: 16)),
+              SliverToBoxAdapter(
+                child: BlocBuilder<TafsirCubit, TafsirByAyahState>(
+                  builder: (context, state) {
+                    if (state is TafsirByAyahError) {
+                      return customErrorWidget(
+                        onPressed: () {
+                          context.read<TafsirCubit>().fetchAyahTafsir(
+                            widget.verse.toString(),
+                            widget.tafsirIdentifier,
+                          );
+                        },
+                      );
+                    } else if (state is TafsirByAyahLoaded) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          textDirection: TextDirection.rtl,
+                          textAlign: TextAlign.justify,
+                          state.tafsirByAyah.data!.text.toString(),
+                          style: context.textTheme.titleLarge,
+                          // textAlign: TextAlign.justify,
+                        ),
+                      );
+                    }
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
