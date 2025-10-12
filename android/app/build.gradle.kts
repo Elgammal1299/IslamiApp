7c7a862fed7cbb8d0fdfaf1d906d6388adad54f3
Ahmed Elgammal <elgammal2024@gmail.com>
2025-10-12 22:11:35 +0300

    chore: Increment versionCode to 7 in build.gradle.kts and update version to 1.1.3 in pubspec.yaml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.wartaqi.islamiapp"
         minSdk = flutter.minSdkVersion   
         targetSdk = flutter.targetSdkVersion 
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.2"
     }
 
