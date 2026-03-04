0742234c25033e8c29868b67cf5962a5adc05105
Ahmed Elgammal <elgammal2024@gmail.com>
2026-03-04 22:30:32 +0200

    feat: update version code to 6 and increment pubspec version to 1.1.2+4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.islamic.wartaqi.app"
         minSdk = flutter.minSdkVersion   
         targetSdk = flutter.targetSdkVersion 
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
     }
 
